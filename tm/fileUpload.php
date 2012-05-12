426b90c55647cdb7dbb611f1380738aed4844a66
Sanjib Narzary <o-._.-o@live.com>
2012-05-12 15:42:35 +0530

    Update tm/fileUpload.php

--- tm/fileUpload.php
+++ tm/fileUpload.php
@@ -1,4 +1,14 @@
 <?php
+/*
+<!--
+        @author: Sanjib Narzary
+        @email: o-._.-o@live.com or alayaran@gmail.com
+        @profession: M. Tech Student
+        @colleges: NIT Silchar, B.Tech and NIT Calicut, M.Tech 2012
+        @experienced: None
+        @license: GNU/GPL
+//-->
+*/
   header('location: editPage.php?id=' . $_POST['id'] . '');
 include('mysql.php');
 if($_POST['id']){
