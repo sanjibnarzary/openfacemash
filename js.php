75f61bba4dc04f34a9c8770408c760f746f94414
Sanjib Narzary <o-._.-o@live.com>
2012-05-12 15:31:55 +0530

    added authors info

--- js.php
+++ js.php
@@ -1,5 +1,13 @@
 <?php $htmlString= 'testing'; ?>
 <html>
+<!--
+        @author: Sanjib Narzary
+        @email: o-._.-o@live.com or alayaran@gmail.com
+        @profession: M. Tech Student
+        @colleges: NIT Silchar, B.Tech and NIT Calicut, M.Tech 2012
+        @experienced: None
+        @license: GNU/GPL
+//-->
   <body>
     <script type="text/javascript">  
       // notice the quotes around the ?php tag         
