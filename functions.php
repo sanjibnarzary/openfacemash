abcc45f96e3b1950ca03adeecea1afe995cb2595
Sanjib Narzary <o-._.-o@live.com>
2012-05-12 15:30:11 +0530

    added authors info

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
-
+/*
+<!--
+        @author: Sanjib Narzary
+        @email: o-._.-o@live.com or alayaran@gmail.com
+        @profession: M. Tech Student
+        @colleges: NIT Silchar, B.Tech and NIT Calicut, M.Tech 2012
+        @experienced: None
+        @license: GNU/GPL
+//-->
+*/
 // Calculate the expected % outcome
 function expected($Rb, $Ra) {
 	return 1/(1 + pow(10, ($Rb-$Ra)/400));
