981f1cb41df30c31a4b075bb58a14e27740a8d4f
Sanjib Narzary <o-._.-o@live.com>
2012-05-12 15:41:02 +0530

    Update tm/editPage.php

--- tm/editPage.php
+++ tm/editPage.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<!--
+        @author: Sanjib Narzary
+        @email: o-._.-o@live.com or alayaran@gmail.com
+        @profession: M. Tech Student
+        @colleges: NIT Silchar, B.Tech and NIT Calicut, M.Tech 2012
+        @experienced: None
+        @license: GNU/GPL
+//-->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>FancyKart Admin</title>
 
